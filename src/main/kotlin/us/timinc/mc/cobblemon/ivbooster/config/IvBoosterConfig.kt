586d63d6e66b567c8c4c6ebbf70aa8cf333de9a1
tmetcalfe89 <tmetcalfe89@gmail.com>
2023-08-22 08:12:51 -0700

    removed config comments Fix #4

--- src/main/kotlin/us/timinc/mc/cobblemon/ivbooster/config/IvBoosterConfig.kt
+++ src/main/kotlin/us/timinc/mc/cobblemon/ivbooster/config/IvBoosterConfig.kt
@@ -5,16 +5,16 @@ import draylar.omegaconfig.api.Config
 import us.timinc.mc.cobblemon.ivbooster.IvBooster
 
 class IvBoosterConfig : Config {
-    @Comment("The number of points each of these counter types grant")
+//    @Comment("The number of points each of these counter types grant")
     val koStreakPoints = 0
     val koCountPoints = 0
     val captureStreakPoints = 1
     val captureCountPoints = 0
 
-    @Comment("The distance at which a spawning Pokemon considers a player")
+//    @Comment("The distance at which a spawning Pokemon considers a player")
     val effectiveRange = 64
 
-    @Comment("Thresholds for the KO counts : shiny chance bonus")
+//    @Comment("Thresholds for the KO counts : shiny chance bonus")
     val thresholds: Map<Int, Int> = mutableMapOf(Pair(5, 1), Pair(10, 2), Pair(20, 3), Pair(30, 4))
 
     override fun getName(): String {
